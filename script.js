d2b07374826ed90d79dcccab364b13c83ff3758a
ThomasCandille <thomascandille0508@gmail.com>
2023-09-13 12:50:33 +0200

    🚧 added pages + bit of js

---
+++ script.js
@@ -0,0 +1,12 @@
+const button_quentin = document.getElementById("button_quentin")
+const button_thomas = document.getElementById("button_thomas")
+
+
+
+button_quentin.addEventListener("click",() => {
+  location.href = "quentin.html"
+})
+
+button_thomas.addEventListener("click",() => {
+  location.href = "thomas.html"
+})
